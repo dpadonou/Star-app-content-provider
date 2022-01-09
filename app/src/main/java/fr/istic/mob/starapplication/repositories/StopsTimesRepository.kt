a7196ca52d7cd4b51785e317cc8a5725deafc2d6
Dieu-Donné PADONOU <57896757+Abnegazar@users.noreply.github.com>
2022-01-09 16:51:53 +0100

    update temps de remplissage

--- app/src/main/java/fr/istic/mob/starapplication/repositories/StopsTimesRepository.kt
+++ app/src/main/java/fr/istic/mob/starapplication/repositories/StopsTimesRepository.kt
@@ -12,6 +12,7 @@ class StopsTimesRepository(private val stopsTimesDAO:StopsTimesDAO) {
     suspend fun addAllStopsTimes(stopsTimes:List<StopTimes>){
         stopsTimesDAO.insertAllStopsTimes(stopsTimes)
     }
+
     /*suspend fun deleteAllStopsTimes(){
        stopsTimesDAO.deleteAllStopsTimes()
     }*/
